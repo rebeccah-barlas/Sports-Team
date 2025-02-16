c7665fb54c0a9e8ee3eb3b6f51fd0ca09e6ec897
rebeccah-barlas <rebeccah.m.barlas@gmail.com>
2025-02-16 17:51:20 -0500

    need console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,4 +41,9 @@ export class AppComponent implements OnInit {
   getActivePlayers(): Player[] {
     return this.team.getActivePlayers();
   }
+
+  logActivePlayers(): void {
+    this.team.logActivePlayers();
+
+  }
 }
